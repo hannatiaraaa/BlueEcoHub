a695b7392cde5a0587a893111b5868aa1bbec3a5
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-12-26 00:56:29 +0700

    Create PrimaryButton component

---
+++ src/components/buttons/PrimaryButton.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  type ColorValue,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  DimensionValue,
+} from 'react-native';
+import {COLOR} from 'configs/colors';
+import {ms} from 'react-native-size-matters';
+import {GlobalText} from 'components/texts/GlobalText';
+import {FontWeight} from 'types/components/GlobalText.type';
+
+type StyleProps = {
+  backgroundColor?: COLOR | ColorValue;
+  width?: DimensionValue | number;
+  borderColor?: COLOR | ColorValue;
+};
+
+interface Props extends TouchableOpacityProps, StyleProps {
+  title?: string;
+  color?: COLOR;
+}
+
+const PrimaryButton = ({
+  backgroundColor = COLOR.PRIMARY_BLUE,
+  title,
+  color = COLOR.WHITE,
+  style,
+  width = '100%',
+  borderColor,
+}: Props) => {
+  const styles = useStyles({backgroundColor, width, borderColor});
+  return (
+    <TouchableOpacity activeOpacity={0.6} style={[styles.button, style]}>
+      <GlobalText
+        textAlign="center"
+        size={ms(16)}
+        type={FontWeight._600}
+        color={color}>
+        {title}
+      </GlobalText>
+    </TouchableOpacity>
+  );
+};
+
+export default PrimaryButton;
+
+const useStyles = (styleProps: StyleProps) =>
+  StyleSheet.create({
+    button: {
+      paddingHorizontal: ms(4),
+      borderRadius: ms(12),
+      paddingVertical: ms(8),
+      borderWidth: styleProps.borderColor ? 1 : 0,
+      ...styleProps,
+    },
+  });
