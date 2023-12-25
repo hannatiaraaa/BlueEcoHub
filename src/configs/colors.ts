b7b437d0b95866ce40f3d045200f6439a9aef4aa
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-12-26 00:54:19 +0700

    Add configs

---
+++ src/configs/colors.ts
@@ -0,0 +1,32 @@
+export enum COLOR {
+  CYAN_BLUE = '#233C52',
+  DARK_TEXT = '#1E232C',
+  DEEP_BLUE = '#051751',
+  LIGHT_BACKGROUND = '#EAECEF',
+  LIGHT_BLUE = '#CBE0F8',
+  NAVY_BLUE = '#074999',
+  PRIMARY_BLUE = '#2280CF',
+  WHITE = '#FFF',
+}
+
+export enum TRANSPARENCY_HEX_COLOR {
+  _05 = '0D',
+  _10 = '1A',
+  _15 = '26',
+  _20 = '33',
+  _25 = '40',
+  _30 = '4D',
+  _35 = '59',
+  _40 = '66',
+  _45 = '73',
+  _50 = '80',
+  _55 = '8C',
+  _60 = '99',
+  _65 = 'A6',
+  _70 = 'B3',
+  _75 = 'BF',
+  _80 = 'CC',
+  _85 = 'D9',
+  _90 = 'E6',
+  _95 = 'F2',
+}
