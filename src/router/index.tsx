06e15f219a6ba83e5a3fe3de76ab7ae7c46a87a2
Hanna Tiara Andarlia <hannatiara@gmail.com>
2023-12-26 00:53:57 +0700

    Initial setup and add fonts

---
+++ src/router/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {MainRoutes} from './routes';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const {Navigator, Screen} = createStackNavigator();
+
+function Router() {
+  return (
+    <Navigator initialRouteName="Explore">
+      {MainRoutes.map(value => {
+        return (
+          <Screen
+            key={value.name}
+            name={value.name}
+            component={value.component}
+            options={value.options}
+          />
+        );
+      })}
+    </Navigator>
+  );
+}
+
+export default Router;
---
+++ src/router/routes.ts
@@ -0,0 +1,9 @@
+import Landing from 'screens/Landing';
+
+export const MainRoutes = [
+  {
+    name: 'Landing',
+    component: Landing,
+    options: {headerShown: false},
+  },
+];
